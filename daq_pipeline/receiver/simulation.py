be0f3eb58a367a59e030b8c5bdb412d8c289fa45
Andrej Babic <andrej.babic@psi.ch>
2019-10-28 16:46:44 +0100

    Add simulation data generator with frequency and data shape

--- daq_pipeline/receiver/simulation.py
+++ daq_pipeline/receiver/simulation.py
@@ -1,14 +1,23 @@
 import logging
+from random import randint
+from time import time, sleep
+
+import numpy
+
+from daq_pipeline import config
 
 _logger = logging.getLogger('SimulatedReceiver')
 
 
 class SimulatedReceiver(object):
-    def __init__(self, device_name, channels_metadata):
+    def __init__(self, device_name, channels_metadata, target_read_time=0.03):
         self.device_name = device_name
         self.channels_metadata = channels_metadata
+        self.target_read_time = target_read_time
 
         _logger.info("Simulating device_name=%s with channels_metadata=%s", self.device_name, self.channels_metadata)
+
+        self.current_pulse_id = int(time())
 
     def __enter__(self):
         pass
@@ -16,20 +25,47 @@ class SimulatedReceiver(object):
     def __exit__(self, ex_type, ex_value, traceback):
         pass
 
+    def _generate_data(self, shape, type):
+
+        raw_data = numpy.zeros(shape=shape, dtype=type)
+
+        random_value = randint(0, 100)
+        raw_data += random_value
+
+        return raw_data.tobytes()
+
     def get_data(self):
-        pulse_id = int(time())
+        start_time = time()
 
-        shape = [2048, 2048]
-        type = "uint16"
-        encoding = "little"
-        compression = None
+        self.current_pulse_id += 1
+        pulse_id = self.current_pulse_id
 
-        pulse_id = pulse_id + 1
+        data = []
 
-        raw_data = numpy.random.rand(*shape)
-        raw_data *= 100
-        raw_data = raw_data.astype(type).tobytes()
+        for channel_metadata in self.channels_metadata:
+            channel_name = channel_metadata["name"]
+            shape = channel_metadata["shape"]
+            type = channel_metadata["type"]
+            encoding = channel_metadata["encoding"]
+            compression = channel_metadata["compression"]
 
-        data = (self.device_name, pulse_id % 10000000, channel_name, pulse_id, raw_data, type, shape, encoding, compression)
+            raw_data = self._generate_data(shape, type)
+
+            data.append(
+                (self.device_name,
+                 pulse_id % config.CHANNEL_DATA_PARTITION_MODULO,
+                 channel_name,
+                 pulse_id,
+                 raw_data,
+                 type,
+                 shape,
+                 encoding,
+                 compression)
+            )
+
+        delta = (time() - start_time) - self.target_read_time
+
+        if delta < 0:
+            sleep(-delta)
 
         return data
