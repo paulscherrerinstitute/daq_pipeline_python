104bc32e107df1cfffd5ce16e636a2de49aaedeb
Andrej Babic <andrej.babic@psi.ch>
2019-10-28 09:58:15 +0100

    Add logging to the bsread stub

--- daq_pipeline/receiver/bsread.py
+++ daq_pipeline/receiver/bsread.py
@@ -2,12 +2,16 @@ import logging
 
 _logger = logging.getLogger('BsreadReceiver')
 
+
 class BsreadReceiver(object):
 
     def __init__(self, source_address, connection_mode, receive_timeout):
         self.source_address = source_address
         self.connection_mode = connection_mode
         self.receive_timeout = receive_timeout
+
+        _logger.info("Connecting to source_address=%s and connection_mode=$s with receive_timeout=%s",
+                     self.source_address, self.connection_mode, self.receive_timeout)
 
     def __enter__(self):
         self.connect()
@@ -16,13 +20,12 @@ class BsreadReceiver(object):
         self.close()
 
     def connect(self):
+        _logger.debug("Connecting to bsread stream %s", self.source_address)
         pass
 
     def close(self):
+        _logger.debug("Closing bsread stream %s.", self.source_address)
         pass
 
     def get_data(self):
-        from time import sleep
-        sleep(1)
-
-        return 12345, {'meta':'data'}
+        pass
