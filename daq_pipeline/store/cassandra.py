c37009eb76cf69a3ca1db886f8c6025ef137bbcd
Andrej Babic <andrej.babic@psi.ch>
2019-10-28 10:42:10 +0100

    Add connection handling to CassandraStore

--- daq_pipeline/store/cassandra.py
+++ daq_pipeline/store/cassandra.py
@@ -1,10 +1,13 @@
 import logging
 
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+
 _logger = logging.getLogger('CassandraStore')
 
 
 INSERT_STATEMENT = """
-INSERT INTO channel_data
+INSERT INTO bsread_data.channel_data
     (device_name, pulse_id_mod, channel_name, pulse_id, data, type, shape, encoding, compression)
 VALUES
     (?, ?, ?, ?, ?, ?, ?, ?, ?)
@@ -22,6 +25,10 @@ class CassandraStore(object):
 
         _logger.debug("Using INSERT_STATEMENT: %s", INSERT_STATEMENT)
 
+        self.cluster = None
+        self.session = None
+        self.prep_insert_statement = None
+
     def __enter__(self):
         self.connect()
 
@@ -30,11 +37,25 @@ class CassandraStore(object):
 
     def connect(self):
         _logger.debug("Connecting to %s", self.nodes_addresses)
-        pass
+
+        self.cluster = Cluster(self.nodes_addresses)
+        self.session = self.cluster.connect()
+
+        self.prep_insert_statement = self.session.prepare(INSERT_STATEMENT)
+        self.prep_insert_statement.consistency_level = ConsistencyLevel.ANY
 
     def close(self):
         _logger.debug("Disconnect from Cassandra")
-        pass
+
+        if self.session:
+            self.session.shutdown()
+
+        if self.cluster:
+            self.cluster.shutdown()
+
+        self.cluster = None
+        self.session = None
+        self.prep_insert_statement = None
 
     def save(data):
         pass
