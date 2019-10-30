08a36026f140ccdefb5201194fdf27160146922a
Andrej Babic <andrej.babic@psi.ch>
2019-10-30 10:48:20 +0100

    Print basic repetition rate

--- daq_pipeline/stats/logstash.py
+++ daq_pipeline/stats/logstash.py
@@ -1,4 +1,5 @@
 import logging
+from time import time
 
 _logger = logging.getLogger('LogstashStats')
 
@@ -8,6 +9,9 @@ class LogstashStats(object):
     def __init__(self, logstash_address, stats_send_interval):
         self.logstash_address = logstash_address
         self.stats_send_interval = stats_send_interval
+
+        self.start_time = None
+        self.n_messages = 0
 
     def __enter__(self):
         self.connect()
@@ -22,4 +26,16 @@ class LogstashStats(object):
         pass
 
     def add(self, data, metadata):
-        pass
+
+        if self.start_time is None:
+            self.start_time = time()
+
+        self.n_messages += 1
+        current_time = time()
+
+        if current_time-self.start_time > self.stats_send_interval:
+
+            print("Processing rate(Hz):", self.n_messages/self.stats_send_interval)
+
+            self.n_messages = 0
+            self.start_time = current_time
