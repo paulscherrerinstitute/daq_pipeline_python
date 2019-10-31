dea90a9d1e639161e7fe9df8e10c66f7cd8ae6cf
Andrej Babic <andrej.babic@psi.ch>
2019-10-31 10:56:38 +0100

    Add tests for repetition rate

--- tests/test_stats_logstash.py
+++ tests/test_stats_logstash.py
@@ -11,16 +11,19 @@ class StatsLogstashTest(unittest.TestCase):
         stats = {
             "stat1": 10,
             "stat2": 20,
-            "stat3": 30
+            "stat3": 30,
+            "iteration": 0
         }
 
         for i in range(10):
             stash._add_stats_to_cache(stats)
 
-        stats = stash._get_stats_from_cache()
+        stats = stash._get_stats_from_cache(2)
 
-        self.assertTrue(stats["stat1"], 10.0)
-        self.assertTrue(stats["stat2"], 20.0)
-        self.assertTrue(stats["stat3"], 30.0)
+        self.assertEqual(stats["stat1"], 10.0)
+        self.assertEqual(stats["stat2"], 20.0)
+        self.assertEqual(stats["stat3"], 30.0)
+        self.assertEqual(stats["rep_rate"], 5)
 
         self.assertDictEqual({}, stash._get_stats_from_cache())
+        self.assertDictEqual({}, stash._get_stats_from_cache(1))
