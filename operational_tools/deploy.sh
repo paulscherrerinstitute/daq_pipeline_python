5cf59cedce72e4ae171bd8b5ce7ea67aa1b894e6
Andrej Babic <andrej.babic@ringlspil.com>
2020-02-24 11:38:53 +0100

    Add bash line

--- operational_tools/deploy.sh
+++ operational_tools/deploy.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 N_PODS=${1:-999}
 echo "Creating first" "${N_PODS}" " pipelines."
 ls -ld sources/* | awk '{print $9}' | head -n ${N_PODS} | xargs -L 1 kubectl --insecure-skip-tls-verify="true" apply -f
